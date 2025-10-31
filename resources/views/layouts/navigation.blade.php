cd7531d4dfa227d31f91431d9dcbd182baed75a3
LailaNounouh <laila.nounouh@student.ehb.be>
2025-10-31 13:38:00 +0100

    Add admin 'Nieuw bericht' link in navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,16 +1,13 @@
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
@@ -19,6 +16,14 @@
                     <x-nav-link :href="route('news.index')" :active="request()->routeIs('news.*')">
                         {{ __('News') }}
                     </x-nav-link>
+
+                    @auth
+                        @if(Auth::user()->is_admin)
+                            <x-nav-link :href="route('news.create')" :active="request()->routeIs('news.create')">
+                                {{ __('Nieuw bericht') }}
+                            </x-nav-link>
+                        @endif
+                    @endauth
 
                     <x-nav-link :href="route('faq.index')" :active="request()->routeIs('faq.*')">
                         {{ __('FAQ') }}
@@ -30,7 +35,6 @@
                 </div>
             </div>
 
-            <!-- Settings Dropdown / Login Register -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 @if(Auth::check())
                     <x-dropdown align="right" width="48">
@@ -50,7 +54,6 @@
                                 {{ __('Profile') }}
                             </x-dropdown-link>
 
-                            <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
                                 <x-dropdown-link :href="route('logout')"
@@ -72,7 +75,6 @@
                 @endif
             </div>
 
-            <!-- Hamburger (mobile menu) -->
             <div class="-me-2 flex items-center sm:hidden">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -84,7 +86,6 @@
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
@@ -95,6 +96,14 @@
                 {{ __('News') }}
             </x-responsive-nav-link>
 
+            @auth
+                @if(Auth::user()->is_admin)
+                    <x-responsive-nav-link :href="route('news.create')" :active="request()->routeIs('news.create')">
+                        {{ __('Nieuw bericht') }}
+                    </x-responsive-nav-link>
+                @endif
+            @endauth
+
             <x-responsive-nav-link :href="route('faq.index')" :active="request()->routeIs('faq.*')">
                 {{ __('FAQ') }}
             </x-responsive-nav-link>
@@ -104,7 +113,6 @@
             </x-responsive-nav-link>
         </div>
 
-        <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             @if(Auth::check())
                 <div class="px-4">
@@ -138,4 +146,3 @@
         </div>
     </div>
 </nav>
-
