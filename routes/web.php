70a7686c604d6a07f87fe381d7516bc99435a75e
LailaNounouh <laila.nounouh@student.ehb.be>
2025-11-16 01:16:40 +0100

    BugFix

--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,9 @@ use App\Http\Controllers\FaqQuestionController;
 use App\Http\Controllers\Admin\FaqController as AdminFaqController;
 use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 
+// Publieke routes
 Route::get('/', [NewsController::class, 'index'])->name('home');
-
 Route::get('/news', [NewsController::class, 'index'])->name('news.index');
-
-Route::middleware(['auth', \App\Http\Middleware\IsAdmin::class])->group(function () {
-    Route::get('/news/create', [NewsController::class, 'create'])->name('news.create');
-    Route::post('/news', [NewsController::class, 'store'])->name('news.store');
-    Route::get('/news/{news}/edit', [NewsController::class, 'edit'])->name('news.edit');
-    Route::put('/news/{news}', [NewsController::class, 'update'])->name('news.update');
-    Route::delete('/news/{news}', [NewsController::class, 'destroy'])->name('news.destroy');
-    Route::resource('faqs', FaqController::class)->except(['index']);
-});
-
 Route::get('/news/{news}', [NewsController::class, 'show'])->name('news.show');
 
 Route::get('/faq', [FaqController::class, 'index'])->name('faq.index');
@@ -31,20 +21,33 @@ Route::post('/faq/question', [FaqQuestionController::class, 'store'])->name('faq
 Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 
+// Dashboard
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Profiel
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
 });
 
-
-
+// Admin routes (enkel admins)
 Route::middleware(['auth', 'is_admin'])->prefix('admin')->name('admin.')->group(function () {
+
+    // News CRUD (admin)
+    Route::get('/news/create', [NewsController::class, 'create'])->name('news.create');
+    Route::post('/news', [NewsController::class, 'store'])->name('news.store');
+    Route::get('/news/{news}/edit', [NewsController::class, 'edit'])->name('news.edit');
+    Route::put('/news/{news}', [NewsController::class, 'update'])->name('news.update');
+    Route::delete('/news/{news}', [NewsController::class, 'destroy'])->name('news.destroy');
+
+    // Admin FAQ
     Route::resource('faqs', AdminFaqController::class)->except(['show']);
+
+    // Admin Categories
     Route::resource('categories', AdminCategoryController::class)->except(['show']);
 });
 
 require __DIR__ . '/auth.php';
+
