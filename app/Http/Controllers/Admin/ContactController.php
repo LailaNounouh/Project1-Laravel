6937c378fddd9dc41d848255b11fcf008db1c811
LailaNounouh <laila.nounouh@student.ehb.be>
2025-11-24 21:06:55 +0100

    Add admin panel for managing contact messages

---
+++ app/Http/Controllers/Admin/ContactController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+
+        $contacts = Contact::orderBy('created_at', 'desc')->paginate(10);
+
+        return view('admin.contacts.index', compact('contacts'));
+    }
+
+    public function show(Contact $contact)
+    {
+        return view('admin.contacts.show', compact('contact'));
+    }
+}
+
