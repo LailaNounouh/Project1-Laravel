62dd9c3a927f95fa515eb52704a079aa9a81be35
LailaNounouh <laila.nounouh@student.ehb.be>
2025-11-24 23:05:31 +0100

    search functionality to news index

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -10,10 +10,21 @@ use Illuminate\Support\Facades\Storage;
 class NewsController extends Controller
 {
 
-    public function index()
+
+    public function index(Request $request)
     {
-        $news = News::latest()->paginate(10);
-        return view('news.index', compact('news'));
+        $query = News::query()->latest();
+
+        if ($search = $request->input('q')) {
+            $query->where(function ($q) use ($search) {
+                $q->where('title', 'like', "%{$search}%")
+                    ->orWhere('content', 'like', "%{$search}%");
+            });
+        }
+
+        $news = $query->paginate(10)->withQueryString();
+
+        return view('news.index', compact('news', 'search'));
     }
 
 
