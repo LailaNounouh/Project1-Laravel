1ad481e1a7bd80c0bdc8e2a52e372ec544b4bb7d
LailaNounouh <laila.nounouh@student.ehb.be>
2025-11-30 12:40:58 +0100

    fix(routes): resolved 404 on news detail + reorganized route structure

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\FaqController;
@@ -11,43 +13,58 @@ use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Admin\ContactController as AdminContactController;
 use App\Http\Controllers\CommentController;
 
+
+
+
 Route::get('/', [NewsController::class, 'index'])->name('home');
 
+
 Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+Route::get('/news/{news}', [NewsController::class, 'show'])->name('news.show'); // FIX voor 404
 
-
-Route::middleware(['auth', \App\Http\Middleware\IsAdmin::class])->group(function () {
-    Route::get('/news/create', [NewsController::class, 'create'])->name('news.create');
-    Route::get('/news/{news}', [NewsController::class, 'show'])->name('news.show');
-    Route::post('/news', [NewsController::class, 'store'])->name('news.store');
-    Route::get('/news/{news}/edit', [NewsController::class, 'edit'])->name('news.edit');
-    Route::put('/news/{news}', [NewsController::class, 'update'])->name('news.update');
-    Route::delete('/news/{news}', [NewsController::class, 'destroy'])->name('news.destroy');
-});
 
 Route::get('/faq', [FaqController::class, 'index'])->name('faq.index');
+
+
 Route::post('/faq', [FaqController::class, 'store'])->name('faq.store');
 Route::post('/faq/question', [FaqQuestionController::class, 'store'])->name('faq.question.store');
 
+
 Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
+
 
 Route::get('/dashboard', fn () => view('dashboard'))
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
 
+
 Route::middleware('auth')->group(function () {
+
+
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+
+
+    Route::post('/news/{news}/comments', [CommentController::class, 'store'])->name('comments.store');
 });
+
 
 
 Route::middleware(['auth', \App\Http\Middleware\IsAdmin::class])
     ->prefix('admin')
     ->name('admin.')
     ->group(function () {
+
+
+        Route::get('/news/create', [NewsController::class, 'create'])->name('news.create');
+        Route::post('/news', [NewsController::class, 'store'])->name('news.store');
+        Route::get('/news/{news}/edit', [NewsController::class, 'edit'])->name('news.edit');
+        Route::put('/news/{news}', [NewsController::class, 'update'])->name('news.update');
+        Route::delete('/news/{news}', [NewsController::class, 'destroy'])->name('news.destroy');
 
 
         Route::resource('faqs', AdminFaqController::class)->except(['show']);
@@ -62,8 +79,5 @@ Route::middleware(['auth', \App\Http\Middleware\IsAdmin::class])
     });
 
 
-Route::post('/news/{news}/comments', [CommentController::class, 'store'])
-    ->middleware('auth')
-    ->name('comments.store');
 
 require __DIR__ . '/auth.php';
