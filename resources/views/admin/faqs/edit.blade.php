87daac8922bd5fcb92c873ec9173c2b0e1975dd1
LailaNounouh <laila.nounouh@student.ehb.be>
2025-12-08 21:50:40 +0100

    Updated FAQ edit page to use new soft GlamConnect form layout

--- resources/views/admin/faqs/edit.blade.php
+++ resources/views/admin/faqs/edit.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="max-w-3xl mx-auto py-8">
-        <h1 class="text-3xl font-bold mb-6 text-pink-600">FAQ bewerken</h1>
+    <div class="max-w-3xl mx-auto py-10">
 
-        <form method="POST" action="{{ route('admin.faqs.update', $faq) }}">
-            @csrf
-            @method('PUT')
+        <h1 class="text-3xl font-bold text-pink-600 mb-6">FAQ bewerken</h1>
 
-            @include('admin.faqs._form')
-        </form>
+        <div class="bg-white rounded-xl shadow p-6 border border-gray-100">
+            <form method="POST" action="{{ route('admin.faqs.update', $faq) }}">
+                @csrf
+                @method('PUT')
+
+                @include('admin.faqs._form')
+            </form>
+        </div>
+
     </div>
 @endsection
