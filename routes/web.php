688cb8b57ed3611883e2accddd86044fc59257f7
LailaNounouh <laila.nounouh@student.ehb.be>
2025-12-26 18:56:31 +0100

    feat: add route for password update

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\FaqController;
@@ -13,54 +12,40 @@ use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Admin\ContactController as AdminContactController;
 use App\Http\Controllers\CommentController;
 
-
-
-
 Route::get('/', [NewsController::class, 'index'])->name('home');
 
-
 Route::get('/news', [NewsController::class, 'index'])->name('news.index');
-Route::get('/news/{news}', [NewsController::class, 'show'])->name('news.show'); // FIX voor 404
-
+Route::get('/news/{news}', [NewsController::class, 'show'])->name('news.show');
 
 Route::get('/faq', [FaqController::class, 'index'])->name('faq.index');
-
-
 Route::post('/faq', [FaqController::class, 'store'])->name('faq.store');
 Route::post('/faq/question', [FaqQuestionController::class, 'store'])->name('faq.question.store');
 
-
 Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
-
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
-
-
 Route::middleware('auth')->group(function () {
-
 
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
 
+    // ✅ NIEUW: wachtwoord updaten
+    Route::post('/profile/password', [ProfileController::class, 'updatePassword'])
+        ->name('profile.password.update');
 
-
-
-Route::post('/news/{news}/comments', [CommentController::class, 'store'])->name('comments.store');});
-
-
+    Route::post('/news/{news}/comments', [CommentController::class, 'store'])
+        ->name('comments.store');
+});
 
 Route::middleware(['auth', \App\Http\Middleware\IsAdmin::class])
     ->prefix('admin')
     ->name('admin.')
     ->group(function () {
-
 
         Route::get('/news/create', [NewsController::class, 'create'])->name('news.create');
         Route::post('/news', [NewsController::class, 'store'])->name('news.store');
@@ -68,18 +53,13 @@ Route::middleware(['auth', \App\Http\Middleware\IsAdmin::class])
         Route::put('/news/{news}', [NewsController::class, 'update'])->name('news.update');
         Route::delete('/news/{news}', [NewsController::class, 'destroy'])->name('news.destroy');
 
-
         Route::resource('faqs', AdminFaqController::class)->except(['show']);
-
-
         Route::resource('categories', AdminCategoryController::class)->except(['show']);
-
 
         Route::get('contacts', [AdminContactController::class, 'index'])->name('contacts.index');
         Route::get('contacts/{contact}', [AdminContactController::class, 'show'])->name('contacts.show');
         Route::delete('contacts/{contact}', [AdminContactController::class, 'destroy'])->name('contacts.destroy');
     });
 
-
-
 require __DIR__ . '/auth.php';
+
