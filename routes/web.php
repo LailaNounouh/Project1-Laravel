ec761e1b8a6a157236ad3f6ecd47f0e3af352b8b
LailaNounouh <laila.nounouh@student.ehb.be>
2025-12-26 21:21:32 +0100

    refactor: clean up and organize web routes

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\FaqController;
-use App\Http\Controllers\ContactController;
 use App\Http\Controllers\FaqQuestionController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\CommentController;
+
 use App\Http\Controllers\Admin\FaqController as AdminFaqController;
 use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Admin\ContactController as AdminContactController;
-use App\Http\Controllers\CommentController;
+
+
 
 Route::get('/', [NewsController::class, 'index'])->name('home');
 
@@ -19,47 +22,78 @@ Route::get('/news/{news}', [NewsController::class, 'show'])->name('news.show');
 
 Route::get('/faq', [FaqController::class, 'index'])->name('faq.index');
 Route::post('/faq', [FaqController::class, 'store'])->name('faq.store');
-Route::post('/faq/question', [FaqQuestionController::class, 'store'])->name('faq.question.store');
+Route::post('/faq/question', [FaqQuestionController::class, 'store'])
+    ->name('faq.question.store');
 
 Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+
 Route::middleware('auth')->group(function () {
 
-    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+    // Profile
+    Route::get('/profile', [ProfileController::class, 'show'])
+        ->name('profile.show');
 
-    // ✅ NIEUW: wachtwoord updaten
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::post('/profile/update', [ProfileController::class, 'update'])
+        ->name('profile.update');
+
     Route::post('/profile/password', [ProfileController::class, 'updatePassword'])
         ->name('profile.password.update');
 
+    // Comments
     Route::post('/news/{news}/comments', [CommentController::class, 'store'])
         ->name('comments.store');
 });
+
+
 
 Route::middleware(['auth', \App\Http\Middleware\IsAdmin::class])
     ->prefix('admin')
     ->name('admin.')
     ->group(function () {
 
-        Route::get('/news/create', [NewsController::class, 'create'])->name('news.create');
-        Route::post('/news', [NewsController::class, 'store'])->name('news.store');
-        Route::get('/news/{news}/edit', [NewsController::class, 'edit'])->name('news.edit');
-        Route::put('/news/{news}', [NewsController::class, 'update'])->name('news.update');
-        Route::delete('/news/{news}', [NewsController::class, 'destroy'])->name('news.destroy');
+        // News management
+        Route::get('/news/create', [NewsController::class, 'create'])
+            ->name('news.create');
 
+        Route::post('/news', [NewsController::class, 'store'])
+            ->name('news.store');
+
+        Route::get('/news/{news}/edit', [NewsController::class, 'edit'])
+            ->name('news.edit');
+
+        Route::put('/news/{news}', [NewsController::class, 'update'])
+            ->name('news.update');
+
+        Route::delete('/news/{news}', [NewsController::class, 'destroy'])
+            ->name('news.destroy');
+
+        // FAQ & categories
         Route::resource('faqs', AdminFaqController::class)->except(['show']);
         Route::resource('categories', AdminCategoryController::class)->except(['show']);
 
-        Route::get('contacts', [AdminContactController::class, 'index'])->name('contacts.index');
-        Route::get('contacts/{contact}', [AdminContactController::class, 'show'])->name('contacts.show');
-        Route::delete('contacts/{contact}', [AdminContactController::class, 'destroy'])->name('contacts.destroy');
+        // Contacts
+        Route::get('/contacts', [AdminContactController::class, 'index'])
+            ->name('contacts.index');
+
+        Route::get('/contacts/{contact}', [AdminContactController::class, 'show'])
+            ->name('contacts.show');
+
+        Route::delete('/contacts/{contact}', [AdminContactController::class, 'destroy'])
+            ->name('contacts.destroy');
     });
 
-require __DIR__ . '/auth.php';
 
+
+require __DIR__ . '/auth.php';
