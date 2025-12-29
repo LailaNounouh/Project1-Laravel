829cad81dbdb3be0ba77b89df8effbb9ef79715f
LailaNounouh <laila.nounouh@student.ehb.be>
2025-12-29 10:30:40 +0100

    Add email view for contact messages

---
+++ resources/views/emails/contact-message.blade.php
@@ -0,0 +1,9 @@
+
+<h2>Nieuw contactbericht</h2>
+
+<p><strong>Naam:</strong> {{ $contact->name }}</p>
+<p><strong>Email:</strong> {{ $contact->email }}</p>
+
+<p><strong>Bericht:</strong></p>
+<p>{{ $contact->message }}</p>
+
